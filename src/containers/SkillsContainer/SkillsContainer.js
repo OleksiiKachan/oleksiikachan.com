53f8c87f2e2e0b3bdf23562b46129342092a8895
OleksiiKachan <alexkachan.lutsk@gmail.com>
2018-09-24 14:41:12 -0400

    Create continer for skills section component

---
+++ src/containers/SkillsContainer/SkillsContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SkillsSection from '../../components/SkillsSection/SkillsSection.js'
+
+/*
+    <SkillsContainer skills={} languages={}/>
+*/
+class SkillsContainer extends Component {
+    render() {
+        return (
+            <SkillsSection skills={this.props.skills} languages={this.props.languages}/>
+        );
+    }
+}
+
+export default SkillsContainer;
