c31447fcd9759ca962f3234ede4a995c2167eaac
OleksiiKachan <alexkachan.lutsk@gmail.com>
2018-09-24 10:59:54 -0400

    Exclude data from container and make them props

--- src/containers/GeneralInformationContainer/GeneralInformationContainer.js
+++ src/containers/GeneralInformationContainer/GeneralInformationContainer.js
@@ -2,44 +2,12 @@ import React, { Component } from 'react';
 import GeneralInformationSection from '../../components/GeneralInformationSection/GeneralInformationSection.js'
 
 /*
-    <GeneralInformationContainer/>
+    <GeneralInformationContainer generalInformation={}/>
 */
 class GeneralInformationContainer extends Component {
-    constructor(props) {
-        super(props);
-
-        this.generalInformation = {
-            name: 'Oleksii Kachan',
-            title: 'UI/UX Developer & Fullstack Software Engineer',
-            photo: '../images/avatar.png',
-            dob: '03 April 1996',
-            phone: '+1 (437) 778-5921',
-            email: 'alexkachan.lutsk@gmail.com',
-            bio: 'Hello there! I am a professional software engineer. My passion is helping business to focus only on business - enterprise automation on higher levels, e.g. MES & ERP System design & development. Also Helping business to improve business indicators by getting more customers & sales by implementing marketing projects, e.g. Landing pages.',
-            resources: [
-                {
-                    link: 'http://www.linkedin.com/in/oleksiikachan',
-                    resource: 'linkedin',
-                },
-                {
-                    link: 'https://www.facebook.com/oleksiikachan',
-                    resource: 'facebook',
-                },
-                {
-                    link: 'https://www.instagram.com/oleksii.kachan/',
-                    resource: 'instagram',
-                },
-                {
-                    link: 'https://vk.com/oleksiikachan',
-                    resource: 'vk',
-                }
-            ]
-        };
-    }
-
     render() {
         return (
-        <GeneralInformationSection generalInformation={this.generalInformation} />
+            <GeneralInformationSection generalInformation={this.props.generalInformation} />
         );
     }
 }
