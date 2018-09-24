13c5f6c78a7db7fd0ce8115c2711e5e0c83f6a27
OleksiiKachan <alexkachan.lutsk@gmail.com>
2018-09-24 11:01:21 -0400

    Create component for header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import './Header.css';
+import HeadMenu from '../HeadMenu/HeadMenu.js'
+
+/*
+    <Header name='' title=''/>
+*/
+class Header extends Component {
+    render() {
+        return (
+            <header className={classNames('header')}>
+                <div className={classNames('headContent', 'header__content')}>
+                    <div className={classNames('headContent__filter')}>
+                        <div className={classNames('headTitle header__title')}>
+                            <h1 className={classNames('headTitle__name')}>{this.props.name}</h1>
+                            <h2 className={classNames('headTitle__title')}>{this.props.title}</h2>
+                        </div>
+                        <div className={classNames('header__button')}>
+                            <a href='#contact' className={classNames('actionButton')}>Hire Me</a>
+                        </div>
+                    </div>
+                </div>
+                <HeadMenu className={classNames('header__menu')}/>
+            </header>
+        );
+    }
+}
+
+export default Header;
