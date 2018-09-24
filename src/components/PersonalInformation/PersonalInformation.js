8377bf19f797f34296f6dbc150e52fdebc854c6f
OleksiiKachan <alexkachan.lutsk@gmail.com>
2018-09-24 00:50:18 -0400

    Add component with personal information

---
+++ src/components/PersonalInformation/PersonalInformation.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import './PersonalInformation.css';
+
+/*
+    <PersonalInformation information={} className='' />
+*/
+class PersonalInformation extends Component {
+    render() {
+        return (
+            <div className={classNames('personalInformation', this.props.className)}>
+                <h3 className={classNames('mainInformation__header')}>PERSONAL INFORMATION</h3>
+                    <ul className={classNames('personalInformation__list')}>
+                        <li className={classNames('personalInformation__listItem')}>
+                            <p>Name:</p>
+                            <p>{this.props.information.name}</p>
+                        </li>
+                        <li className={classNames('personalInformation__listItem')}>
+                            <p>Date of Birth:</p>
+                            <p>{this.props.information.dob}</p>
+                        </li>
+                        <li className={classNames('personalInformation__listItem')}>
+                            <p>Phone:</p>
+                            <p>{this.props.information.phone}</p>
+                        </li>
+                        <li className={classNames('personalInformation__listItem')}>
+                            <p>Email:</p>
+                            <p>{this.props.information.email}</p>
+                        </li>
+                    </ul>
+                </div>
+        );
+    }
+}
+
+export default PersonalInformation;
