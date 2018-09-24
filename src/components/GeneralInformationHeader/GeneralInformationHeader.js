ad1f08ffbd639750e7f09b21d7e06a0c93a461b2
OleksiiKachan <alexkachan.lutsk@gmail.com>
2018-09-24 00:49:36 -0400

    Add component of general information header

---
+++ src/components/GeneralInformationHeader/GeneralInformationHeader.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import './GeneralInformationHeader.css';
+import SocialMediaButtonPanel from '../SocialMediaButtonPanel/SocialMediaButtonPanel'
+import avatar from '../../images/avatar.png'
+
+/*
+    <GeneralInformationHeader generalInformation = {} className='' />
+*/
+class GeneralInformationHeader extends Component {
+    render() {
+        return (
+            <div className={classNames('generalInformationHeader', this.props.className)}>
+                <div className={classNames('avatar', 'generalInformationHeader__avatar')}>
+                    <figure className={classNames('avatar__filter')}>
+                        <img alt='Oleksii Kachan' src={avatar} className={classNames('avatar__image')}/>
+                    </figure>
+                </div>
+                <div className={classNames('generalInformationHeader_side')}>
+                    <div className={classNames('generalInformationHeader__placeHolder')}></div>
+                    <div className={classNames('generalInformationHeader__head')}>
+                        <h1 className={classNames('generalInformationHeader__name')}>{this.props.generalInformation.name}</h1>
+                        <h2 className={classNames('generalInformationHeader__position')}>{this.props.generalInformation.title}</h2>
+                    </div>
+                    <SocialMediaButtonPanel resources={this.props.generalInformation.resources} className={classNames('generalInformationHeader__socialMedia')}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default GeneralInformationHeader;
