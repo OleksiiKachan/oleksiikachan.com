3d9cb814b945348adc89e71bb2c38c1b4ba530c7
OleksiiKachan <alexkachan.lutsk@gmail.com>
2018-09-26 16:26:40 -0400

    Add form action

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -3,31 +3,31 @@ import classNames from 'classnames';
 import './ContactForm.css';
 
 /*
-    <ContactForm onSubmit={} />
+    <ContactForm />
 */
 class ContactForm extends Component {
     render() {
         return (
             <div className={classNames('contactFormContainer')}>
-                <form id='contactForm' className={classNames('contactForm')} action='' method='post'>
+                <form id='contactForm' className={classNames('contactForm')}  action='https://formspree.io/alexkachan.lutsk@gmail.com' method='post'>
                     <h3 className={classNames('contactForm__title')}>Contact me</h3>
                     <fieldset className={classNames('contactForm__contactField')}>
-                        <input className={classNames('contactForm__contactControl')} placeholder="Your name" type="text" tabindex="1" required autofocus/>
+                        <input className={classNames('contactForm__contactControl')} placeholder="Your name" type="text" tabindex="1" name='name' required autofocus/>
                     </fieldset>
                     <fieldset className={classNames('contactForm__contactField')}>
-                        <input className={classNames('contactForm__contactControl')} placeholder="Your Email Address" type="email" tabindex="2" required/>
+                        <input className={classNames('contactForm__contactControl')} placeholder="Your Email Address" type="email" tabindex="2" name='email' required/>
                     </fieldset>
                     <fieldset className={classNames('contactForm__contactField')}>
-                        <input className={classNames('contactForm__contactControl')} placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required/>
+                        <input className={classNames('contactForm__contactControl')} placeholder="Your Phone Number (optional)" type="tel" tabindex="3" name='phone'/>
                     </fieldset>
                     <fieldset className={classNames('contactForm__contactField')}>
-                        <input className={classNames('contactForm__contactControl')} placeholder="Your Web Site (optional)" type="url" tabindex="4" required/>
+                        <input className={classNames('contactForm__contactControl')} placeholder="Your Web Site (optional)" type="url" tabindex="4" name='site'/>
                     </fieldset>
                     <fieldset className={classNames('contactForm__contactField')}>
-                        <textarea className={classNames('contactForm__contactControl', 'contactForm__contactControl_multipleRow')}  placeholder="Type your message here...." tabindex="5" required></textarea>
+                        <textarea className={classNames('contactForm__contactControl', 'contactForm__contactControl_multipleRow')}  placeholder="Type your message here...." tabindex="5" name='message'></textarea>
                     </fieldset>
                     <fieldset className={classNames('contactForm__contactField', 'contactForm__contactField_button')}>
-                        <button className={classNames('contactForm__contactControl', 'contactForm__submitButton')} name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+                        <button className={classNames('contactForm__contactControl', 'contactForm__submitButton')} type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
                     </fieldset>
                 </form>
             </div>
