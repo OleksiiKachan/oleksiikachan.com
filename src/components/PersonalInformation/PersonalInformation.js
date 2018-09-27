a792f2616527d705153ff958de502dafceb4c938
OleksiiKachan <alexkachan.lutsk@gmail.com>
2018-09-26 23:45:18 -0400

    Remove duplications

--- src/components/PersonalInformation/PersonalInformation.js
+++ src/components/PersonalInformation/PersonalInformation.js
@@ -6,27 +6,44 @@ import './PersonalInformation.css';
     <PersonalInformation information={} className='' />
 */
 class PersonalInformation extends Component {
+    constructor(props) {
+        super(props);
+
+        this.information = [
+            {
+                title: 'Name',
+                value: this.props.information.name
+            },
+            {
+                title: 'Date of Birth',
+                value: this.props.information.dob
+            },
+            {
+                title: 'Phone',
+                value: this.props.information.contacts.phone
+            },
+            {
+                title: 'Email',
+                value: this.props.information.contacts.email
+            },
+        ];
+    }
+
     render() {
         return (
             <div className={classNames('personalInformation', this.props.className)}>
                 <h3 className={classNames('mainInformation__header')}>PERSONAL INFORMATION</h3>
                     <ul className={classNames('personalInformation__list')}>
-                        <li className={classNames('personalInformation__listItem')}>
-                            <p>Name:</p>
-                            <p>{this.props.information.name}</p>
-                        </li>
-                        <li className={classNames('personalInformation__listItem')}>
-                            <p>Date of Birth:</p>
-                            <p>{this.props.information.dob}</p>
-                        </li>
-                        <li className={classNames('personalInformation__listItem')}>
-                            <p>Phone:</p>
-                            <p>{this.props.information.contacts.phone}</p>
-                        </li>
-                        <li className={classNames('personalInformation__listItem')}>
-                            <p>Email:</p>
-                            <p>{this.props.information.contacts.email}</p>
-                        </li>
+                    {
+                        this.information.map(informationItem => {
+                            return(
+                                <li className={classNames('personalInformation__listItem')}>
+                                    <p>{informationItem.title}:</p>
+                                    <p>{informationItem.value}</p>
+                                </li>
+                            );
+                        })
+                    }
                     </ul>
                 </div>
         );
