5c8f145cc625421db78050339c93f245dfb37681
OleksiiKachan <alexkachan.lutsk@gmail.com>
2018-09-26 23:19:56 -0400

    Separate component for language panel

---
+++ src/components/LanguagesPanel/LanguagesPanel.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import './LanguagesPanel.css';
+import FilledStar from './filledStar.png';
+import BlankStar from './blankStar.png';
+
+/*
+    <LanguagesPanel languages={} className='' />
+*/
+class LanguagesPanel extends Component {
+    createLanguageRate = (rate) => {
+        let visualRate = []
+        rate = rate > 5 ? 5 : rate;
+
+        for (let i = 0; i < rate; i++) {
+            visualRate.push(<img src={FilledStar} alt='Filled blue star.' className={classNames('languageItem__circleRate')}/>)
+        }
+
+        for (let i = 0; i < 5-rate; i++) {
+            visualRate.push(<img src={BlankStar} alt='Blank blue star.'  className={classNames('languageItem__circleRate')}/>)
+        }
+
+        return visualRate;
+    }
+
+    render() {
+        return (
+            <ul className={classNames(this.props.className)}>
+            {
+                this.props.languages.map(language => {
+                    return (  
+                        <li className={classNames('languageItem')}>
+                            <p className={classNames('languageItem__name')}>{language.language}</p>
+                            <p className={classNames('languageItem__rate')}>
+                            {
+                                this.createLanguageRate(language.stars)
+                            }
+                            </p>
+                        </li>
+                    );
+                })
+            }
+            </ul>
+        );
+    }
+}
+
+export default LanguagesPanel;
