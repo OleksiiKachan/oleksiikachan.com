1b1387d5b9c1edde6f945ee13c358e6943d896ea
OleksiiKachan <alexkachan.lutsk@gmail.com>
2018-12-30 12:37:56 -0500

    feat(containers): adjust container to new header style

--- src/containers/HeaderContainer/HeaderContainer.tsx
+++ src/containers/HeaderContainer/HeaderContainer.tsx
@@ -5,6 +5,38 @@ import Header from '../../components/Header';
     <HeaderContainer name='' title=''/>
 */
 class HeaderContainer extends Component<any, any> {
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const headCover: any = document.getElementsByClassName('headCover')[0];
+    const headCoverHeight: number = headCover!.clientHeight;
+
+    const headMenu: any = document.getElementsByClassName('headMenu')[0];
+    const headMenuHeight: number = headMenu!.clientHeight;
+    const headMenu__logo: any = document.getElementsByClassName(
+      'headMenu__logo'
+    )[0];
+    const headMenu__list: any = document.getElementsByClassName(
+      'headMenu__list'
+    )[0];
+    console.log(`${window.pageYOffset} - ${headCoverHeight}`);
+    if (window.pageYOffset > headCoverHeight) {
+      headMenu!.classList.add('headMenu_scrolled');
+      headMenu__logo!.classList.add('headMenu__logo_scrolled');
+      headMenu__list!.classList.add('headMenu__list_scrolled');
+    } else if (window.pageYOffset < headCoverHeight + headMenuHeight) {
+      headMenu!.classList.remove('headMenu_scrolled');
+      headMenu__logo!.classList.remove('headMenu__logo_scrolled');
+      headMenu__list!.classList.remove('headMenu__list_scrolled');
+    }
+  }
+
   render() {
     return (
       <Header
