95e768479031b0eab05c8c6462520dfd69c22c58
OleksiiKachan <alexkachan.lutsk@gmail.com>
2018-12-30 12:36:34 -0500

    feat(components): make header change style on scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,19 +13,18 @@ class Header extends Component<any, any> {
         id="header"
         className={classNames('header', this.props.className)}
       >
-        <div className={classNames('headContent', 'header__content')}>
-          <div className={classNames('headContent__filter')}>
-            <div className={classNames('headTitle header__title')}>
-              <h1 className={classNames('headTitle__name')}>
-                {this.props.name}
-              </h1>
-              <h2 className={classNames('headTitle__title')}>
+        <div className={classNames('header__headCover', 'headCover')}>
+          <div className={classNames('headCover_filter')}>
+            <img
+              src="https://res.cloudinary.com/oleksiikachan/image/upload/v1546142703/portfolio/images/logo/logo_horizontal_light.svg"
+              className={classNames('headCover__logo')}
+            />
+            <div className={classNames('headCover__titleWrapper')}>
+              <h1 className={classNames('headCover__title')}>
                 {this.props.title}
-              </h2>
-            </div>
-            <div className={classNames('header__button')}>
-              <a href="#contact" className={classNames('actionButton')}>
-                Hire Me
+              </h1>
+              <a href="#portfolio" className={classNames('headCover__link')}>
+                Go to portfolio
               </a>
             </div>
           </div>
