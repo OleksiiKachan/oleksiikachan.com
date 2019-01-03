12bef32d5593fdaca8deb4f86a96b42d295b7353
OleksiiKachan <alexkachan.lutsk@gmail.com>
2019-01-02 21:11:21 -0500

    feat(components): update header design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,21 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
+import classnames from 'classnames';
 import './Header.scss';
-import HeadMenu from '../HeadMenu';
+import HeaderCover from '../HeaderCover';
+import Navigation from '../Navigation';
 
 /*
-    <Header name='' title=''/>
+    <Header name='' title='' navigationItems=[]/>
 */
 class Header extends Component<any, any> {
   render() {
     return (
       <header
         id="header"
-        className={classNames('header', this.props.className)}
+        className={classnames('header', this.props.className)}
       >
-        <div className={classNames('header__headCover', 'headCover')}>
-          <div className={classNames('headCover_filter')}>
-            <img
-              src="https://res.cloudinary.com/oleksiikachan/image/upload/v1546142703/portfolio/images/logo/logo_horizontal_light.svg"
-              className={classNames('headCover__logo')}
-            />
-            <div className={classNames('headCover__titleWrapper')}>
-              <h1 className={classNames('headCover__title')}>
-                {this.props.title}
-              </h1>
-              <a href="#portfolio" className={classNames('headCover__link')}>
-                Go to portfolio
-              </a>
-            </div>
-          </div>
-        </div>
-        <HeadMenu className={classNames('header__menu')} />
+        <HeaderCover name={this.props.name} title={this.props.title} />
+        <Navigation navigationItems={this.props.navigationItems} />
       </header>
     );
   }
