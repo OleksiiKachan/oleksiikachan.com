8ae29f4c2778d652a190d17e5f01d4951497ca57
OleksiiKachan <alexkachan.lutsk@gmail.com>
2019-01-12 01:59:27 -0500

    feat(components): implement new design for Contact Form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -8,76 +8,60 @@ import './ContactForm.scss';
 class ContactForm extends Component<any, any> {
   render() {
     return (
-      <div className={classNames('contactFormContainer')}>
+      <div className={classNames('contactFormContainer', this.props.className)}>
+        <h2 className={classNames('contactFormContainer__title')}>
+          Contact Me
+        </h2>
         <form
-          id="contactForm"
           className={classNames('contactForm')}
+          autoComplete="off"
           action="https://formspree.io/alexkachan.lutsk@gmail.com"
           method="post"
         >
-          <h3 className={classNames('contactForm__title')}>Contact me</h3>
-          <fieldset className={classNames('contactForm__contactField')}>
+          <div>
+            <div className={classNames('contactForm__field')}>
+              <input
+                type="text"
+                name="title"
+                className={classNames('contactForm__input')}
+                autoComplete="off"
+                required
+              />
+              <label className={classNames('contactForm__label')}>
+                Your name
+              </label>
+            </div>
+            <div className={classNames('contactForm__field')}>
+              <input
+                type="text"
+                name="contact"
+                className={classNames('contactForm__input')}
+                autoComplete="off"
+                required
+              />
+              <label className={classNames('contactForm__label')}>
+                Your email
+              </label>
+            </div>
+          </div>
+          <div className={classNames('contactForm__field')}>
             <input
-              className={classNames('contactForm__contactControl')}
-              placeholder="Your name"
               type="text"
-              name="name"
-              required
-            />
-          </fieldset>
-          <fieldset className={classNames('contactForm__contactField')}>
-            <input
-              className={classNames('contactForm__contactControl')}
-              placeholder="Your Email Address"
-              type="email"
-              name="email"
-              required
-            />
-          </fieldset>
-          <fieldset className={classNames('contactForm__contactField')}>
-            <input
-              className={classNames('contactForm__contactControl')}
-              placeholder="Your Phone Number (optional)"
-              type="tel"
-              name="phone"
-            />
-          </fieldset>
-          <fieldset className={classNames('contactForm__contactField')}>
-            <input
-              className={classNames('contactForm__contactControl')}
-              placeholder="Your Web Site (optional)"
-              type="url"
-              name="site"
-            />
-          </fieldset>
-          <fieldset className={classNames('contactForm__contactField')}>
-            <textarea
-              className={classNames(
-                'contactForm__contactControl',
-                'contactForm__contactControl_multipleRow'
-              )}
-              placeholder="Type your message here...."
               name="message"
+              className={classNames('contactForm__input')}
+              autoComplete="off"
+              required
             />
-          </fieldset>
-          <fieldset
-            className={classNames(
-              'contactForm__contactField',
-              'contactForm__contactField_button'
-            )}
-          >
-            <button
-              className={classNames(
-                'contactForm__contactControl',
-                'contactForm__submitButton'
-              )}
-              type="submit"
-              id="contact-submit"
-              data-submit="...Sending"
-            >
-              Submit
-            </button>
-          </fieldset>
+            <label className={classNames('contactForm__label')}>
+              Your message
+            </label>
+          </div>
+          <input
+            type="submit"
+            name="submit"
+            value="Submit"
+            className={classNames('contactForm__submitButton')}
+          />
         </form>
       </div>
     );
