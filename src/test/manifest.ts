bde7e02de5ffdbfa07ae5f3c01171cda5cb18d97
OleksiiKachan <alexkachan.lutsk@gmail.com>
2019-04-11 18:55:37 -0400

    test: adjust test route manifest to new components
    add OutlineButton route
    change ExperienceTimelineSection props

--- src/test/manifest.ts
+++ src/test/manifest.ts
@@ -1,37 +1,48 @@
 // components added to the manifest can be accessed in browser via /test/{componentName}
 
 export default {
+  OutlineButton: {
+    components: require('../components/OutlineButton'),
+    props: {
+      onClick: () => {},
+      children: 'See More',
+    },
+  },
   ExperienceTimelineSection: {
     components: require('../components/ExperienceTimelineSection'),
     props: {
       timeline: [
         {
-          time: '2014',
           title:
+            'National Technical University of Ukraine "Kyiv Polytechnic Institute"',
+          subtitle: 'Bachelor of Software Engineering',
+          dates: '2013-2017',
+          type: 'education',
+        },
+        {
+          title: 'Ltd. "Kontynium-Treyd"',
+          subtitle:
             'Technician Programmer of administrative and economic department',
-          description: 'Ltd. "Kontynium-Treyd"',
+          dates: '2014',
+          type: 'work',
         },
         {
-          time: '2016-2017',
-          title: 'Software and Systems Engineer',
-          description: 'rLoop Incorporated',
+          title: 'rLoop Incorporated',
+          subtitle: 'Software and Systems Engineer',
+          dates: '2016-2017',
+          type: 'work',
         },
         {
-          time: '2013-2017',
-          title: 'Bachelor of Software Engineering',
-          description:
-            'National Technical University of Ukraine "Kyiv Polytechnic Institute", Faculty of Information and Computer Science, Technical Cybernetics Department',
+          title: 'OLSOM L.L.C',
+          subtitle: 'Software Engineer',
+          dates: '2017',
+          type: 'work',
         },
         {
-          time: '2017',
-          title: 'Software Engineer',
-          description: 'OLSOM L.L.C',
-        },
-        {
-          time: '2017-2019',
-          title: 'Postgraduate Degree',
-          description:
-            'The Humber Institute of Technology and Advanced Learning,School of Media Studies & Information Technology, Information Technology Solutions',
+          title: 'The Humber Institute of Technology and Advanced Learning',
+          subtitle: 'Ontario Graduate Certificate',
+          dates: '2017-2019',
+          type: 'education',
         },
       ],
     },
