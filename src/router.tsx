34b42bbef648c753339d43a6469462e974baa2b9
OleksiiKachan <alexkachan.lutsk@gmail.com>
2019-04-10 20:22:05 -0400

    refactor: change path to general scss

--- src/router.tsx
+++ src/router.tsx
@@ -8,7 +8,7 @@ import MainLayout from './layouts/MainLayout';
 import App from './containers/App';
 
 // General styles
-import './style/general.scss';
+import './assets/style/index.scss';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
