c13ef09fd3945bbc23f918c47a36578ee566ddcd
OleksiiKachan <alexkachan.lutsk@gmail.com>
2019-04-15 11:30:42 -0400

    test(components): change import from OutlineButton.tsx to index.ts

--- src/components/OutlineButton/OutlineButton.test.tsx
+++ src/components/OutlineButton/OutlineButton.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import OutlineButton from './OutlineButton';
+import OutlineButton from './';
 
-describe('MyComponent', () => {
+describe('OutlineButton', () => {
   it('should render correctly in "debug" mode', () => {
     const component = shallow(
       <OutlineButton onClick={() => {}}>Caption</OutlineButton>
