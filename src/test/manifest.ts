99ff4a6ef2b1c10801ff668a985ad28c8b3a869e
OleksiiKachan <alexkachan.lutsk@gmail.com>
2019-04-20 08:32:26 -0400

    refactor: update test manifest for updated TimelineSection

--- src/test/manifest.ts
+++ src/test/manifest.ts
@@ -9,40 +9,63 @@ export default {
     },
   },
   ExperienceTimelineSection: {
-    components: require('../components/ExperienceTimelineSection'),
+    components: require('../components/TimelineSection'),
     props: {
-      timeline: [
+      experience: [
         {
-          title:
-            'National Technical University of Ukraine "Kyiv Polytechnic Institute"',
-          subtitle: 'Bachelor of Software Engineering',
-          dates: '2013-2017',
-          type: 'education',
-        },
-        {
-          title: 'Ltd. "Kontynium-Treyd"',
-          subtitle:
+          id: 'kontynium-treyd',
+          companyTitle: 'Ltd. "Kontynium-Treyd"',
+          position:
             'Technician Programmer of administrative and economic department',
           dates: '2014',
-          type: 'work',
+          responsibilities: [
+            'System administration of the enterprise using 1C: Enterprise (ERP-like platform)',
+            'Maintenance of computers, peripheral and specialized equipment (common printers, Zebra printers, electronic scales, fiscal cash registers, etc.)',
+          ],
         },
         {
-          title: 'rLoop Incorporated',
-          subtitle: 'Software and Systems Engineer',
+          id: 'rloop',
+          companyTitle: 'rLoop Incorporated',
+          position: 'Software and Systems Engineer',
           dates: '2016-2017',
-          type: 'work',
+          responsibilities: [
+            'Software & System engineering',
+            'UI & UX Design & Development',
+          ],
+          projects: ['rpod'],
         },
         {
-          title: 'OLSOM L.L.C',
-          subtitle: 'Software Engineer',
+          id: 'olsom',
+          companyTitle: 'OLSOM L.L.C',
+          position: 'Software Engineer',
           dates: '2017',
-          type: 'work',
+          responsibilities: [
+            "Development and support of the company's products: AGW modular complex solution for automotive industry; TDM (Tools Data Manager) for automotive industry",
+            "Development and support of the client projects based on company's products (1-3-tier automotive manufacturers (Volkswagen, Opel, Tesla, etc.)",
+            'Development of outsourced minor projects',
+            'Development of internal tools',
+          ],
+          projects: ['agw', 'ftp-service', 'tdm', 'cardboard-prod'],
+        },
+      ],
+      education: [
+        {
+          id: 'kpi',
+          school:
+            'National Technical University of Ukraine "Kyiv Polytechnic Institute"',
+          degree: 'Bachelor of Software Engineering',
+          department:
+            'Faculty of Informatics and Computer Science, Department of Technical Cybernetics',
+          program: 'Software Engineering',
+          dates: '2013-2017',
         },
         {
-          title: 'The Humber Institute of Technology and Advanced Learning',
-          subtitle: 'Ontario Graduate Certificate',
+          id: 'humber',
+          school: 'The Humber Institute of Technology and Advanced Learning',
+          degree: 'Ontario Graduate Certificate',
+          department: 'School of Media Studies & Information Technology',
+          program: 'Information Technology Solutions',
           dates: '2017-2019',
-          type: 'education',
         },
       ],
     },
