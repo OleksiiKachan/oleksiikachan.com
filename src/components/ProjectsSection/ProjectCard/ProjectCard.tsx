06ae2b56fb722b08a1389057fad81840a2e8b002
OleksiiKachan <alexkachan.lutsk@gmail.com>
2019-05-05 17:14:59 -0400

    feat(components): add shader to card image

--- src/components/ProjectsSection/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectsSection/ProjectCard/ProjectCard.tsx
@@ -27,18 +27,20 @@ export default ({ id, title, stack, coverImage, className }: PropsType) => {
       className={classNames('projectCard', className)}
       style={{ backgroundImage: `url(${coverImage})` }}
     >
-      <div className={classNames('projectCard__inner')}>
-        <div className={classNames('projectCard__title')}>{title}</div>
-        <div className={classNames('projectCard__stack')}>{stack}</div>
-        <Link to={`projects/${id}`}>
-          <OutlineButton
-            className={classNames('projectCard__button')}
-            type="gold"
-            onClick={() => {}}
-          >
-            See More
-          </OutlineButton>
-        </Link>
+      <div className={classNames('projectCard__shader')}>
+        <div className={classNames('projectCard__inner')}>
+          <div className={classNames('projectCard__title')}>{title}</div>
+          <div className={classNames('projectCard__stack')}>{stack}</div>
+          <Link to={`projects/${id}`}>
+            <OutlineButton
+              className={classNames('projectCard__button')}
+              type="gold"
+              onClick={() => {}}
+            >
+              See More
+            </OutlineButton>
+          </Link>
+        </div>
       </div>
     </div>
   );
