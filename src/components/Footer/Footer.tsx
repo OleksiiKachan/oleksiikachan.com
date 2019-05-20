cfa55bf93b0f162b15d4a0a562680a0c04023b3c
OleksiiKachan <alexkachan.lutsk@gmail.com>
2019-05-20 16:05:58 -0400

    feat(components): create footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,42 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import './Footer.scss';
+import SocialMediaButton from '../SocialMediaButton';
 
 /*
     <Footer resources={} className='' />
 */
-class Footer extends Component<any, any> {
-  render() {
-    return (
-      <footer className={classNames('footer', this.props.className)}>
-        <div className={classNames('footer__section', 'footer__linksSection')}>
-          {this.props.resources.map(
-            (resource: { link: string; resource: string }) => {
-              return (
-                <a href={resource.link}>
-                  <i
-                    className={classNames(
-                      'footer__link',
-                      'fa',
-                      `fa-${resource.resource}`
-                    )}
-                  />
-                </a>
-              );
-            }
-          )}
-        </div>
 
-        <div className={classNames('footer__section', 'footer__logoSection')}>
+type PropsType = {
+  smContacts: Array<{ type: string; url: string }>;
+  contacts: {
+    phone: string;
+    email: string;
+    location: string;
+  };
+  className?: string;
+};
+
+export default ({ smContacts, contacts, className }: PropsType) => {
+  return (
+    <footer className={classNames('footerWrapper', className)}>
+      <div className={classNames('footer')}>
+        <div className={classNames('footer__logoWrapper')}>
           <img
+            src="https://res.cloudinary.com/oleksiikachan/image/upload/v1546142703/portfolio/images/logo/logo_horizontal_dark.svg"
             className={classNames('footer__logo')}
-            src="https://res.cloudinary.com/oleksiikachan/image/upload/v1543467884/portfolio/images/logo.png"
-            alt="Oleksii Kachan Logo"
           />
         </div>
-      </footer>
-    );
-  }
-}
-
-export default Footer;
+        <div className={classNames('footer__contacts')}>
+          <div className={classNames('footer__smLinks')}>
+            {smContacts.map(item => {
+              return (
+                <div
+                  key={smContacts.indexOf(item)}
+                  className={classNames('footer__smButton')}
+                >
+                  <SocialMediaButton type={item.type} url={item.url} />
+                </div>
+              );
+            })}
+          </div>
+          <ul className={classNames('footer__contactsList')}>
+            <li className={classNames('footer__contactsItem')}>
+              {contacts.email}
+            </li>
+            <li className={classNames('footer__contactsItem')}>
+              {contacts.phone}
+            </li>
+            <li className={classNames('footer__contactsItem')}>
+              {contacts.location}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
