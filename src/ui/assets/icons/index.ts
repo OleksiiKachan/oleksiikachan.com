32acd16bbc253d351f203419020cf1b37953001e
OleksiiKachan <alexkachan.lutsk@gmail.com>
2019-10-05 12:33:34 -0400

    feat(environment): create ts files for style assets
    files are duplicates of shared scss files

---
+++ src/ui/assets/icons/index.ts
@@ -0,0 +1,53 @@
+import icon_vk_filled from './icon_vk-filled.svg';
+import icon_vk_blank from './icon_vk-blank.svg';
+import icon_telegram_filled from './icon_telegram-filled.png';
+import icon_telegram_blank from './icon_telegram-blank.png';
+import icon_linkedin_filled from './icon_linkedin-filled.svg';
+import icon_linkedin_blank from './icon_linkedin-blank.svg';
+import icon_instagram_filled from './icon_instagram-filled.svg';
+import icon_instagram_blank from './icon_instagram-blank.svg';
+import icon_github_filled from './icon_github-filled.svg';
+import icon_github_blank from './icon_github-blank.svg';
+import icon_facebook_filled from './icon_facebook-filled.svg';
+import icon_facebook_blank from './icon_facebook-blank.svg';
+
+export const icons = {
+  social: {
+    vk: {
+      dark: {
+        filled: icon_vk_filled,
+        blank: icon_vk_blank,
+      },
+    },
+    telegram: {
+      dark: {
+        filled: icon_telegram_filled,
+        blank: icon_telegram_blank,
+      },
+    },
+    linkedin: {
+      dark: {
+        filled: icon_linkedin_filled,
+        blank: icon_linkedin_blank,
+      },
+    },
+    instagram: {
+      dark: {
+        filled: icon_instagram_filled,
+        blank: icon_instagram_blank,
+      },
+    },
+    github: {
+      dark: {
+        filled: icon_github_filled,
+        blank: icon_github_blank,
+      },
+    },
+    facebook: {
+      dark: {
+        filled: icon_facebook_filled,
+        blank: icon_facebook_blank,
+      },
+    },
+  },
+};
