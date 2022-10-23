f82de161f4fb72bd4fdf7513618c76c921946374
OleksiiKachan <okachan@nobul.com>
2022-10-23 19:27:31 -0400

    refactor: move Link prop to engine

--- scenes/app/wonder-engine-config.tsx
+++ scenes/app/wonder-engine-config.tsx
@@ -2,7 +2,8 @@ import Link from 'next/link';
 import type { WonderEngineContext } from 'wonder-engine';
 
 const WonderContextConfig: WonderEngineContext = {
-  Link: (props) => <Link {...props} legacyBehavior={false} />,
+  Link,
+  platform: `nextjs`,
 };
 
 export default WonderContextConfig;
