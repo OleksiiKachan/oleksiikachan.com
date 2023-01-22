98c7eab17f62d7e76f06f47e2208ca531f637a91
OleksiiKachan <okachan@nobul.com>
2023-01-22 18:51:26 -0500

    chore: add svgr

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
   images: {
     domains: [`res.cloudinary.com`],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [`@svgr/webpack`],
+    });
+
+    return config;
+  },
   async redirects() {
     return [
       {
