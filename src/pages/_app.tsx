be8680baa3226841adaf9d855255714cddf0426d
OleksiiKachan <okachan@nobul.com>
2023-04-21 22:51:28 -0400

    chore: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import Client from 'client';
+import { Analytics } from '@vercel/analytics/react';
 
 const isClient = () => typeof window !== `undefined`;
 
@@ -13,19 +14,22 @@ const isBot = () =>
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     {isClient() && !isBot() && (
-      <Script
-        id="gtag-base"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      <>
+        <Script
+          id="gtag-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
           `,
-        }}
-      />
+          }}
+        />
+        <Analytics />
+      </>
     )}
     <Client>
       <Component {...pageProps} />
