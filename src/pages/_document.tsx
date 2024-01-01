7edf7672aa5f184b7867f2e741bacd7d3a94aa53
OleksiiKachan <alexkachan.lutsk@gmail.com>
2024-01-01 16:28:36 -0500

    chore: remove hydration error overlay

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,12 @@
-import Document, { DocumentContext } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import Script from 'next/script';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -21,5 +28,49 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {process.env.NODE_ENV === `development` && (
+            <Script
+              id="disable-error-overlay"
+              strategy="beforeInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `const handler = (event) => {
+                if (event?.error?.message) {
+                  const message = event.error.message.toLowerCase();
+                      if (
+                      message.match(
+                      /firebase|hydration|hydrating|rendered|cancelled|rendering/i
+                      )
+                      ) {
+                        event.stopImmediatePropagation();
+                  }
+                }
+              };
+
+              window.addEventListener('error', handler);
+              window.addEventListener('unhandledrejection', handler);`,
+              }}
+            />
+          )}
+        </Head>
+        <body id="root">
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: `none`, visibility: `hidden` }}
+            />
+          </noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
