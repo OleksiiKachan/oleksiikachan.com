f77e5bf2c61ec35bc31e7bd93087498621e955ed
OleksiiKachan <alexkachan.lutsk@gmail.com>
2024-01-01 16:18:57 -0500

    feat: create `useToggle` hook

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,19 @@
+import { useCallback, useState } from 'react';
+
+const useToggle = (
+  initialState = false
+): [boolean, (value?: unknown) => void] => {
+  const [_isVisible, setIsVisible] = useState<boolean>(initialState);
+
+  const toggleVisibility = useCallback(
+    (value?: unknown) =>
+      typeof value === `boolean`
+        ? setIsVisible(value)
+        : setIsVisible((isVisible) => !isVisible),
+    []
+  );
+
+  return [_isVisible, toggleVisibility];
+};
+
+export default useToggle;
