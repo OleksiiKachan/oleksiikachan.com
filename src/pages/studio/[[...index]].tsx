2f43dcaf85c03efdf9222379003cbb33c19ebf3c
OleksiiKachan <alexkachan.lutsk@gmail.com>
2024-01-09 23:26:04 -0500

    refactor: use `React.FC` for component typing

--- src/pages/studio/[[...index]].tsx
+++ src/pages/studio/[[...index]].tsx
@@ -1,5 +1,3 @@
-'use client';
-
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
@@ -12,6 +10,6 @@
 import { NextStudio } from 'next-sanity/studio';
 import config from '../../../sanity.config';
 
-export default function StudioPage() {
-  return <NextStudio config={config} />;
-}
+const StudioPage = () => <NextStudio config={config} />;
+
+export default StudioPage;
