831f6120d55f45d4bfd733a6adac199db07d666a
OleksiiKachan <alexkachan.lutsk@gmail.com>
2025-03-31 10:29:36 -0400

    chore: remove `swcMinify` field

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { withSentryConfig } = require(`@sentry/nextjs`);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   productionBrowserSourceMaps: true,
   compiler: {
     styledComponents: true,
